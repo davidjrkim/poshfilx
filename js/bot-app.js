0f88c0fda03f9ce629600bf283531e4d92c09ffe
David Kim <davidjrkims@gmail.com>
2020-10-31 09:02:04 +0900

    working on profile selector

--- js/bot-app.js
+++ js/bot-app.js
@@ -1,5 +1,5 @@
 
-// this code does not work on the top of the page in side the <head> so i'm putting this separately at the bottom XD some body pleace explain
+// this code does not work on the top of the page in side the <head> so i'm putting this separately at the bottom XD some body pleace explain ㅠㅠ
 
 var checkbox = document.querySelector("input[name=checkbox]");
 checkbox.addEventListener( 'change', function() {
