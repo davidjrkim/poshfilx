07f657707b480f0cb1cf001913b2bf7e6f126493
David Kim <davidjrkims@gmail.com>
2020-12-07 20:43:06 +0900

    alot of things have been done but its not ready

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let menu = document.querySelector(".menu")
+let profileSselecte = document.getElementById("profile-selecte");
+
+profileSselecte.onclick = function() {
+    // menu.style.visibility = "visible";
+    console.log("f")
+  }
